d69bee3fb8449dbf91cf3e6e99a078fcf6f3267e
Derrick <dward330@gmail.com>
2020-05-04 07:16:29 -0400

    Register names Change from $ to #

--- Services/Registers/Registry.cs
+++ Services/Registers/Registry.cs
@@ -28,26 +28,26 @@ namespace BajanVincyAssembly.Services.Registers
         {
             this.Registers = new Dictionary<string, Register>();
 
-            this.Registers.Add("$temp0", new Register("$temp0"));
-            this.Registers.Add("$temp1", new Register("$temp1"));
-            this.Registers.Add("$temp2", new Register("$temp2"));
-            this.Registers.Add("$temp3", new Register("$temp3"));
-            this.Registers.Add("$temp4", new Register("$temp4"));
-            this.Registers.Add("$temp5", new Register("$temp5"));
-            this.Registers.Add("$temp6", new Register("$temp6"));
-            this.Registers.Add("$temp7", new Register("$temp7"));
-            this.Registers.Add("$temp8", new Register("$temp8"));
-            this.Registers.Add("$temp9", new Register("$temp9"));
-            this.Registers.Add("$bv0", new Register("$bv0"));
-            this.Registers.Add("$bv1", new Register("$bv1"));
-            this.Registers.Add("$bv2", new Register("$bv2"));
-            this.Registers.Add("$bv3", new Register("$bv3"));
-            this.Registers.Add("$bv4", new Register("$bv4"));
-            this.Registers.Add("$bv5", new Register("$bv5"));
-            this.Registers.Add("$bv6", new Register("$bv6"));
-            this.Registers.Add("$bv7", new Register("$bv7"));
-            this.Registers.Add("$bv8", new Register("$bv8"));
-            this.Registers.Add("$bv9", new Register("$bv9"));
+            this.Registers.Add("#temp0", new Register("#temp0"));
+            this.Registers.Add("#temp1", new Register("#temp1"));
+            this.Registers.Add("#temp2", new Register("#temp2"));
+            this.Registers.Add("#temp3", new Register("#temp3"));
+            this.Registers.Add("#temp4", new Register("#temp4"));
+            this.Registers.Add("#temp5", new Register("#temp5"));
+            this.Registers.Add("#temp6", new Register("#temp6"));
+            this.Registers.Add("#temp7", new Register("#temp7"));
+            this.Registers.Add("#temp8", new Register("#temp8"));
+            this.Registers.Add("#temp9", new Register("#temp9"));
+            this.Registers.Add("#bv0", new Register("#bv0"));
+            this.Registers.Add("#bv1", new Register("#bv1"));
+            this.Registers.Add("#bv2", new Register("#bv2"));
+            this.Registers.Add("#bv3", new Register("#bv3"));
+            this.Registers.Add("#bv4", new Register("#bv4"));
+            this.Registers.Add("#bv5", new Register("#bv5"));
+            this.Registers.Add("#bv6", new Register("#bv6"));
+            this.Registers.Add("#bv7", new Register("#bv7"));
+            this.Registers.Add("#bv8", new Register("#bv8"));
+            this.Registers.Add("#bv9", new Register("#bv9"));
         }
 
         /// <summary>
